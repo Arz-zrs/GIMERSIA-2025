8bac0619296bf609d61c3aefad904fa9c3257ad2
Gunturadhtya <gunturpdng@gmail.com>
2025-11-21 18:12:17 +0800

    fix tutorial stage

--- Scenes/Stages/1_Tutorial/tutorial_1.gd
+++ Scenes/Stages/1_Tutorial/tutorial_1.gd
@@ -5,14 +5,14 @@ func _ready() -> void:
 	target_cleared_cube = 15
 
 func get_screen_pos_for_cell(grid_pos: Vector2i) -> Vector2:
-	return tilemap_layer.map_to_local(grid_pos) * TILE_OFFSET + Vector2(41, 29)
+	return tilemap_layer.map_to_local(grid_pos) * TILE_OFFSET
 
 func get_cell_for_global_pos(global_pos: Vector2) -> Vector2i:
-	var local_pos = tilemap_layer.to_local(global_pos) + Vector2(41, 29)
+	var local_pos = tilemap_layer.to_local(global_pos)
 	return tilemap_layer.local_to_map(local_pos)
 
 func get_spawn_pos() -> Vector2i:
-	return Vector2i(2, 9)
+	return Vector2i(2, 10)
 
 func _on_next_button_pressed() -> void:
 	get_tree().change_scene_to_packed(preload("res://Scenes/Stages/2_Tutorial/tutorial_2.tscn"))
