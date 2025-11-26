eb914012a6126c82fbf914c90824efb087293d44
Arya Arrozza Ridho Syaputra <ozzaboy123@gmail.com>
2025-11-26 17:58:19 +0800

    fixed abe pathfinding so it doesnt fall

--- Scripts/2_State/Abe/abe_idle.gd
+++ Scripts/2_State/Abe/abe_idle.gd
@@ -10,13 +10,30 @@ func enter(previous_state_path: String, data := {}) -> void:
 		Vector2i(1, -2), # Left
 		Vector2i(-2, 1)  # Down
 	]
-	_next_move_dir = possible_moves.pick_random()
 	
-	var target_grid_pos = abe.current_grid_pos + _next_move_dir
-	var target_screen_pos = abe.world.get_screen_pos_for_cell(target_grid_pos)
-	abe.move_highlighter.global_position = target_screen_pos
-	abe.move_highlighter.show()
+	# Search for valid grid so it doesn't fall
+	var valid_moves = []
+	for move in possible_moves:
+		var target_grid_pos = abe.current_grid_pos + move
+		
+		# Check if the tile exists and is walkable
+		if abe.world.is_tile_walkable(target_grid_pos):
+			valid_moves.append(move)
+
+	# Assign moves if grid is valid
+	if valid_moves.size() > 0:
+		_next_move_dir = valid_moves.pick_random()
+	else:
+		_next_move_dir = Vector2i.ZERO
+
+		# Show abe move highlighter
+	if _next_move_dir != Vector2i.ZERO:
+		var target_grid_pos = abe.current_grid_pos + _next_move_dir
+		var target_screen_pos = abe.world.get_screen_pos_for_cell(target_grid_pos)
+		abe.move_highlighter.global_position = target_screen_pos
+		abe.move_highlighter.show()
 	
+	# Connect to beat
 	if not GameStates.beat_hit.is_connected(_on_beat_hit):
 		GameStates.beat_hit.connect(_on_beat_hit)
 
@@ -27,11 +44,11 @@ func enter(previous_state_path: String, data := {}) -> void:
 
 func _on_beat_hit(_beat_num: int):
 	var current_turn = GameStates.game_turn
-	if current_turn == _last_moved_turn:
-		return 
-	_last_moved_turn = current_turn
-	abe.move_highlighter.hide()
-	finished.emit(HOPPING, {"move_direction": _next_move_dir})
+	# if 2nd beat, abe moves
+	if current_turn % 2 != 0:
+		_last_moved_turn = current_turn
+		abe.move_highlighter.hide()
+		finished.emit(HOPPING, {"move_direction": _next_move_dir}) 
 
 func exit() -> void:
 	abe.move_highlighter.hide()
